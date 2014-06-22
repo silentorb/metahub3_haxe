6aa125affc9e913449b4617aa13202ce2ffa283c
silentorb <christopherwjohnson@gmail.com>
2014-06-22 17:35:23 -0600

    Trying to add support for including external Haxe modules.

--- post_build.bat
+++ post_build.bat
@@ -4,5 +4,5 @@ if %TARGET% == nodejs (
 	copy "%1\output\nodejs\metahub.js" E:\Websites\vineyard\node_modules\vineyard-metahub\metahub3.js
 )
 if %TARGET% == php (
-	xcopy "%1\output\php\lib" E:\Websites\vineyard-drupal\sites\all\modules\custom\metahub\lib /E /Y
+REM	xcopy "%1\output\php\lib" E:\Websites\vineyard-drupal\sites\all\modules\custom\vineyard\lib\metahub\ /E /Y
 )

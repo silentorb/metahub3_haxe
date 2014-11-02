7a45145bc4290e5cdbf8f26a3070ace568a94307
silentorb <christopherwjohnson@gmail.com>
2014-11-01 20:47:47 -0600

    Refining code that only worked in the lose world of JavaScript.  There is still more refining to do.

--- src/metahub/Utility.hx
+++ src/metahub/Utility.hx
@@ -12,10 +12,10 @@ class Utility {
 #if html5
 	throw new Exception("Not supported with html5.");
 #elseif nodejs
-  json = Nodejs.fs.readFileSync(url, { encoding: 'ascii' });
+  json = Nodejs.fs.readFileSync(url, { encoding: 'ascii' } );	
 #else
-    //throw new Exception("load_json() not supported for this compilation target.");
-		json = sys.io.File.getContent(url);
+    throw new Exception("load_json() not supported for this compilation target.");
+		//json = sys.io.File.getContent(url);
 #end
 
     return Json.parse(json);
